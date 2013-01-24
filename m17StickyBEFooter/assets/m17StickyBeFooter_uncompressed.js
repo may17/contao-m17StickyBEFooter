168b0986744aa9834cef385cc23cea743c12f140
Joe Ray Gregory <j.r.gregory@gmx.de>
2013-01-24 20:58:46 +0100

    Smartbackend theme fix and some other improvements

--- m17StickyBEFooter/assets/m17StickyBeFooter_uncompressed.js
+++ m17StickyBEFooter/assets/m17StickyBeFooter_uncompressed.js
@@ -1,33 +1,28 @@
 /* Stcky Footer for Contao
  * @author: Joe Ray Gregory aka may17
+ * @author: Stefan Melz (Debugbar bugfix)
  * @copyright: Joe Ray Gregory 2012 joe@may17.de
  * @license LGPL
  */
 (function() {
-    var M17StickyFooter = {
+    M17StickyFooter = {
         init: function() {
+
             var formBody = document.getElement('.tl_formbody_submit') || false,
-                pos,
-                formHeight,
-                tt,
-                formBodyPos,
-                difference,
                 setEvent,
-                fieldsetEl,
+                fieldsets,
+                debugToggler,
                 self = this;
 
             //invoke only when the element existists
             if(formBody) {
-                formBodyPos = formBody.getPosition;
-                formHeight = formBody.getHeight();
-                tt = formBodyPos.y + 49;
-                pos = formBody.getPosition().y + formBody.getHeight();
 
-                //load the methoad with init
-                this.setItem(formBody);
+            	this.footerData = this.generateSubmitObj(formBody);
+				this.generateOffset();
+                this.setStickyPosition(formBody);
                 
                	//Prepare event
-               	setEvent = this.setItem.bind(this).pass(formBody);
+               	setEvent = this.setItem.bind(this);
                	
                	//Add resize and scroll events
                 window.addEvents({
@@ -35,45 +30,101 @@
                     'resize': setEvent,
                     'ajax_change': setEvent
                 });
+
+                // Reset the position on window resizing
+               	window.addEvent('resize', this.setStickyPosition.bind(this).pass(formBody));
+                
+                fieldsets = $$('fieldset');
                 
                 //addEvent Delegation for legends
-                fieldsetEl = $$('fieldset');
-                fieldsetEl.addEvent('click:relay(legend)', function(e){
-                    self.setItem(formBody);
+                fieldsets.addEvent('click:relay(legend)', function(e){
+                    self.setItem();
                 });
                 
                 //Improvment for checkbox actions
-                fieldsetEl.addEvent('click:relay(input.tl_checkbox[onclick])', function(e){
-                    self.setItem(formBody);
+                fieldsets.addEvent('click:relay(input.tl_checkbox[onclick])', function(e){
+                    self.setItem();
                 });
+                
+                //Watch debug toggler
+                debugToggler = document.id('tog') || false;
+                if(debugToggler) {
+                    debugToggler.addEvent('click',function(){
+                    	self.setItem();
+                	})
+                }
             }
         },
-        /* initStickyFooter
-         * @desc initialize the stickyFooter for Backend
+        
+        /* generateSubmitObj
+         * @desc generates the internal footer Object
          */
-        setItem: function(formBody) {
-            var raffle = formBody.getPosition().y +49,
-            	diff = raffle - window.getHeight(),
-            	formBodyInner = formBody.getFirst(),
-            	_width = formBodyInner.getStyle('width'),
-				$this = window,
-                $body = document.body,
+        generateSubmitObj: function(formbody) {
+        	var submitContainer = formbody.getFirst();
+        	
+        	return {
+        		item: formbody,
+        		submitContainer: submitContainer,
+        		height: submitContainer.getHeight(),
+        		width: submitContainer.getStyle('width'),
+        		offset: 0,
+        		position: submitContainer.getCoordinates()
+        	}
+        },
+        
+        /* generateOffset
+         * @desc calculates an offset if debug bar is active
+         */
+        generateOffset: function(){
+			var debug = document.getElement('div#debug') || false;
+			
+			
+            //set news offset for debugbar
+            if(debug){
+                var offset = debug.getElement('p').getSize().y + debug.getElement('div').getSize().y -1;
+                    this.footerData.submitContainer.setStyle('bottom', offset);
+                    this.footerData.offset = offset;
+            }
+        },
+
+        /**
+         * Set a position for the sticky footer
+         * @param formbody
+         */
+        setStickyPosition: function(formbody) {
+            this.footerData.submitContainer.setStyle('left', formbody.getPosition().x);
+            console.log('action');
+        },
+        
+        /* setItem
+         *
+         */
+        setItem: function(e) {
+            var offset = this.generateOffset(),
+            	raffle = this.footerData.item.getPosition().y + this.footerData.height + this.footerData.offset,
+            	win = window,
+            	diff = raffle - win.getHeight(),
+                docBody = document.body,
                 stickyClass = 'stickySave',
-            	scrollSizeToInt = $this.getScroll().y.toInt();
+            	scrollSizeToInt = win.getScroll().y.toInt();
             	
             if(scrollSizeToInt > diff) {
-                $body.removeClass(stickyClass);
+                docBody.removeClass(stickyClass);
             } else {
-                //check if class is set
-                if(!$body.hasClass(stickyClass)) {
-                    formBodyInner.setStyle('width', _width);
-                    $body.addClass(stickyClass);
+                //check if class set
+                if(!docBody.hasClass(stickyClass)) {
+                    this.footerData.submitContainer.setStyle('width', this.footerData.width);
+
+                    docBody.addClass(stickyClass);
+                    
                 }
             }
         }
     }
+
     window.addEvent('domready', function() {
         M17StickyFooter.init();
         this.scrollTo(0, 1);
     });
+
 })();
