99eb5123edb23602913ba8024e92f80b1326cdfe
jrgregory <joe@may17.de>
2016-05-26 14:03:14 +0200

    Adding language files

---
+++ m17StickyBEFooter/languages/de/modules.php
@@ -0,0 +1,4 @@
+<?php
+
+$GLOBALS['TL_LANG']['MOD']['m17StickyBEFooter'][0] = 'Sticky Footer für den Speichern Bereich im Backend';
+$GLOBALS['TL_LANG']['MOD']['m17StickyBEFooter'][1] = 'Mit diesem Modul wird der Speichern Bereich an die Unterkante des Browsers positioniert.';
---
+++ m17StickyBEFooter/languages/en/modules.php
@@ -0,0 +1,4 @@
+<?php
+
+$GLOBALS['TL_LANG']['MOD']['m17StickyBEFooter'][0] = 'Sticky save area for backend';
+$GLOBALS['TL_LANG']['MOD']['m17StickyBEFooter'][1] = 'This is a tiny backend extension that sticks the footer to the bottom of the browser as long as the original region isn`t reached.';
